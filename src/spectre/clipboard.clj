7c9eaf00601ea6db32641106e5ec64ddae4c7fa9
Rahul De <rahul080327@gmail.com>
2026-08-16 10:51:07 +0100

    [spectre] add clipboard

---
+++ src/spectre/clipboard.clj
@@ -0,0 +1,24 @@
+(ns spectre.clipboard
+  "Copy to the system clipboard by shelling out."
+  (:require
+   [babashka.fs :as fs]
+   [babashka.process :as p]))
+
+(def ^:private tools
+  [["pbcopy"]
+   ["wl-copy"]
+   ["xclip" "-selection" "clipboard"]
+   ["xsel" "-ib"]
+   ["clip"]])
+
+(defn tool
+  "First available clipboard command, or nil."
+  []) ;; TODO
+
+(defn copy!
+  "Copy s to the clipboard with cmd, by default the first available tool.
+   Returns the command used, or nil when there is none.
+  The value goes over stdin."
+  ([s])) ;; TODO
+
+(comment (copy! "foo"))
